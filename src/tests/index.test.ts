4ab5fa70424a1462662473722c5380e8f3cb1af1
Shreekar11 <shreekargade2004@gmail.com>
2025-03-03 17:39:32 +0530

    feat: cypress testing added

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,67 +1,20 @@
-import { describe, expect, it, vi } from "vitest";
-import request from "supertest";
-import { app } from "../index";
-import { client } from "../db";
+describe('Testing app', () => {
+  beforeEach(() => {
+    cy.visit('https://app.100xdevs.com')
+  })
 
-vi.mock("../db");
+  it('is able to log in', () => {
+    cy.contains('Login').should('exist')
+    cy.contains('Login').click()
+    cy.contains('Signin to your Account').should('exist', { timeout: 10000 })
+    cy.get('#email').type('harkirat.iitr@gmail.com');
 
-describe("POST /sum", () => {
-  it("should return the sum of two numbers", async () => {
-    client.sum.create.mockResolvedValue({
-      id: 1,
-      a: 1,
-      b: 1,
-      result: 3,
-    });
+    // Fill in the password field
+    cy.get('#password').type('123random');
 
-    vi.spyOn(client.sum, "create");
+    cy.get('button').eq(4).click()
 
-    const res = await request(app).post("/sum").send({
-      a: 1,
-      b: 2,
-    });
+    cy.contains('View Content').should("exist", {timeout: 10000})
+  })
 
-    expect(client.sum.create).toHaveBeenCalledWith({
-      data: {
-        a: 1,
-        b: 2,
-        result: 3,
-      },
-    });
-
-    expect(res.statusCode).toBe(200);
-    expect(res.body.answer).toBe(3);
-  });
-
-  it("should return 411 if no inputs are provided", async () => {
-    const res = await request(app).post("/sum").send({});
-    expect(res.statusCode).toBe(411);
-    expect(res.body.message).toBe("Incorrect inputs");
-  });
-});
-
-describe("GET /sum", () => {
-  it("should return the sum of two numbers", async () => {
-    client.sum.create.mockResolvedValue({
-      id: 1,
-      a: 1,
-      b: 1,
-      result: 3,
-    });
-
-    const res = await request(app)
-      .get("/sum")
-      .set({
-        a: "1",
-        b: "2",
-      })
-      .send();
-    expect(res.statusCode).toBe(200);
-    expect(res.body.answer).toBe(3);
-  });
-
-  it("should return 411 if no inputs are provided", async () => {
-    const res = await request(app).get("/sum").send();
-    expect(res.statusCode).toBe(411);
-  });
-});
+})
